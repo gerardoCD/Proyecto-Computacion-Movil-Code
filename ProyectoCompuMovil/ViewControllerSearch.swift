251ceb17528bb947669578a5bf818426de89011a
Gerardo <ferrari_gerardo@live.com.mx>
2018-06-05 10:28:33 -0500

    Search View
    Se finalizo la vista de busqueda

--- ProyectoCompuMovil/ViewControllerSearch.swift
+++ ProyectoCompuMovil/ViewControllerSearch.swift
@@ -7,29 +7,69 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ViewControllerSearch: UIViewController {
-
+    let realm = try! Realm()
+    var boatArray = [Boat]()
+    var contador = 0
+    @IBOutlet weak var imgBoat: UIImageView!
+    @IBOutlet weak var txtDescription: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        let boats = realm.objects(Boat.self)
+        print(boats)
+        for boat in boats{
+            boatArray.append(boat)
+        }
+        imgBoat.image = UIImage(data: boatArray[0].imageBoat)
+        txtDescription.text = boatArray[0].descriptions
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+            boatArray.removeAll()
+        let boats = realm.objects(Boat.self)
+        for boat in boats{
+            boatArray.append(boat)
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
     }
-    */
+    }
 
+  
+    
+    @IBAction func btnNext(_ sender: UIButton) {
+        
+        performSegue(withIdentifier: "segueExtraServices", sender: nil)
+    }
+    
+    @IBAction func swipeLeft(_ sender: UISwipeGestureRecognizer) {
+        if( 0 == contador){
+            imgBoat.image = UIImage(data: boatArray[contador].imageBoat)
+            txtDescription.text = boatArray[contador].descriptions
+            contador = boatArray.count - 1
+        }else if (0 < contador){
+            imgBoat.image = UIImage(data: boatArray[contador].imageBoat)
+            txtDescription.text = boatArray[contador].descriptions
+            contador -= 1
+        }
+        
+        print("izquierda")
+    }
+    
+  
+ 
+    @IBAction func swipeRigth(_ sender: UISwipeGestureRecognizer) {
+        if( boatArray.count - 1  >= contador){
+            imgBoat.image = UIImage(data: boatArray[contador].imageBoat)
+            txtDescription.text = boatArray[contador].descriptions
+            contador += 1
+        }else{
+            contador = 0
+            imgBoat.image = UIImage(data: boatArray[contador].imageBoat)
+            txtDescription.text = boatArray[contador].descriptions
+            contador += 1
+        }
+        print("Derecha")
+    }
+    
 }
